6e56e8469e0883853113f65aeada009159c26cb3
Osamah-Anjum <osamahbinjawaidalanjum@gmail.com>
2026-04-16 11:05:41 +0530

    pushed

--- expenseTrac-frontend/src/axios.js
+++ expenseTrac-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/", // your backend
+  baseURL: "https://expense-trac-backend.onrender.com/api/user", // your backend
 });
 
 // Attach JWT token automatically

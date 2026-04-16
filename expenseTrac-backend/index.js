85a9a68601f70b8b9da3d1cb898bd5590daa97b3
Osamah-Anjum <osamahbinjawaidalanjum@gmail.com>
2026-04-16 11:44:37 +0530

    new commits

--- expenseTrac-backend/index.js
+++ expenseTrac-backend/index.js
@@ -10,11 +10,19 @@ const app = express()
 
 //MIDDLEWARE
 app.use(cors({
-  origin: "https://expensetracker-1-9w5i.onrender.com",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: ["https://expensetracker-1-9w5i.onrender.com"],
   credentials: true
 }))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://expensetracker-1-9w5i.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
